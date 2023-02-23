280017caef92a03d0780380ad7dd98547e719881
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-02-23 15:39:18 +0530

    feat: added fancy mouse thingy

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,11 +2,47 @@
 <script lang="ts">
 	import Section from "$lib/components/ui/Section/Section.svelte";
 	import Page from "$lib/components/fragments/Page/Page.svelte";
+
+	import logo from '$lib/assets/logo.png'
 </script>
+
+<style lang="scss">
+	.sect1 {
+		display: flex;
+		cursor: default;
+	}
+
+	.fore {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translateX(-50%) translateY(-50%);
+
+		font-size: 3rem;
+		text-align: center;
+		text-shadow: 0 0 5px black;
+	}
+
+	.back {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translateX(-50%) translateY(-50%);
+	}
+
+
+</style>
 
 <Page>
 	<Section>
-		<h1>Apratim is here.</h1>
+		<div class="sect1">
+			<div class="back">
+				<img src={logo} alt="apratim logo" />
+			</div>
+			<div class="fore">
+				<h1>Apratim is here.</h1>
+			</div>
+		</div>
 	</Section>
 	<Section>
 		<div class="second"></div>
