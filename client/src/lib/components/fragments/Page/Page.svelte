c2852d6eae3ca94256de66203bb1f4d3a1889667
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-02-23 15:39:18 +0530

    feat: added fancy mouse thingy

--- client/src/lib/components/fragments/Page/Page.svelte
+++ client/src/lib/components/fragments/Page/Page.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
+	import { onMount } from "svelte";
 
-    
+    let m = {x: 0, y: 0}
+
+    let blob: HTMLElement
+
+
+    const handleMouseMove = (event: MouseEvent) => {
+        m.x = event.clientX
+        m.y = event.clientY
+
+        blob.animate({
+            left: `${m.x}px`,
+            top: `${m.y}px`
+        }, {duration: 3000, fill: "forwards"})
+    }
 
 </script>
 
-
-<main>
+<main on:mousemove="{handleMouseMove}">
+    <div id="blob" bind:this={blob}/>
     <slot />
 </main>
 
@@ -18,6 +32,33 @@
 		flex-direction: column;
 		scroll-snap-type: y mandatory;
 		color: #fff;
+    }
+
+    #blob {
+        background: linear-gradient(to right, aquamarine, mediumpurple);
+        height: 500px;
+        aspect-ratio: 1;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        translate: -50% -50%;
+        border-radius: 50%;
+        animation: rotate 20s infinite;
+        filter: blur(200px);
+    }
+
+    @keyframes rotate{
+        from {
+            rotate: 0deg;
+        }
+
+        50% {
+            scale: 1 1.5;
+        }
+
+        to {
+            rotate: 360deg;
+        }
     }
     
 </style>
