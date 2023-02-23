956535b4d9788f3d7d2dc67d9af8a31e22692ee8
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-02-23 08:49:20 +0530

    feat: page fragment

--- client/src/lib/components/fragments/Page/Page.svelte
+++ client/src/lib/components/fragments/Page/Page.svelte
@@ -1,23 +1,17 @@
-<script lang="ts">
-
-    
-
-</script>
-
-
-<main>
-    <slot />
-</main>
-
 <style lang="scss">
-
-    main {
-        width: 100%;
+	main {
+		width: 100%;
 		height: fit-content;
 		display: flex;
 		flex-direction: column;
 		scroll-snap-type: y mandatory;
 		color: #fff;
-    }
-    
+	}
 </style>
+
+<script lang="ts">
+</script>
+
+<main>
+	<slot />
+</main>
