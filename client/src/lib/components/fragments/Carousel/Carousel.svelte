a1d2c0be6f2bc64b343763dc5a8d48ff79206b4a
Sahil Garg <contact.sahilgarg@proton.me>
2023-02-25 00:41:51 +0530

    Added onMount() and removed error.

--- client/src/lib/components/fragments/Carousel/Carousel.svelte
+++ client/src/lib/components/fragments/Carousel/Carousel.svelte
@@ -25,43 +25,46 @@
 
 <script type="ts">
 	import sampleImage from "$lib/assets/sample.webp";
+	import { onMount } from "svelte";
 
 	let track: any;
 
-	window.onmousedown = (e) => {
-		track.dataset.mouseDownAt = e.clientX;
-	};
-	window.onmousemove = (e) => {
-		if (track.dataset.mouseDownAt === "0") return;
-		const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-			maxDelta = window.innerWidth / 2;
-		const percentage = (mouseDelta / maxDelta) * -100,
-			nextPercentage = Math.max(
-				Math.min(parseFloat(track.dataset.prevPercentage) + percentage, 0),
-				-100
-			);
-		track.dataset.percentage = nextPercentage;
-		track.style.transform = `translate(${nextPercentage}%, -50%)`;
-		track.animate(
-			{
-				transform: `translate(${nextPercentage}%, -50%)`
-			},
-			{ duration: 1200, fill: "forwards" }
-		);
-
-		for (const image of track.getElementsByClassName("image")) {
-			image.animate(
+	onMount(() => {
+		window.onmousedown = (e) => {
+			track.dataset.mouseDownAt = e.clientX;
+		};
+		window.onmousemove = (e) => {
+			if (track.dataset.mouseDownAt === "0") return;
+			const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+				maxDelta = window.innerWidth / 2;
+			const percentage = (mouseDelta / maxDelta) * -100,
+				nextPercentage = Math.max(
+					Math.min(parseFloat(track.dataset.prevPercentage) + percentage, 0),
+					-100
+				);
+			track.dataset.percentage = nextPercentage;
+			track.style.transform = `translate(${nextPercentage}%, -50%)`;
+			track.animate(
 				{
-					objectPosition: `${100 + nextPercentage}% center`
+					transform: `translate(${nextPercentage}%, -50%)`
 				},
 				{ duration: 1200, fill: "forwards" }
 			);
-		}
-	};
-	window.onmouseup = () => {
-		track.dataset.mouseDownAt = "0";
-		track.dataset.prevPercentage = track.dataset.percentage;
-	};
+
+			for (const image of track.getElementsByClassName("image")) {
+				image.animate(
+					{
+						objectPosition: `${100 + nextPercentage}% center`
+					},
+					{ duration: 1200, fill: "forwards" }
+				);
+			}
+		};
+		window.onmouseup = () => {
+			track.dataset.mouseDownAt = "0";
+			track.dataset.prevPercentage = track.dataset.percentage;
+		};
+	});
 </script>
 
 <section>
