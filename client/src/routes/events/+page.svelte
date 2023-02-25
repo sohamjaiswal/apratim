6c1e39947b2c37f36dabe79ad183600dba258897
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-02-25 17:33:18 +0530

    feat: modualarized Glossy card

--- client/src/routes/events/+page.svelte
+++ client/src/routes/events/+page.svelte
@@ -3,6 +3,43 @@
     import Section from "$lib/components/ui/Section/Section.svelte";
 	import Page from "$lib/components/fragments/Page/Page.svelte";
 	import GlossyCards from "$lib/components/fragments/GlossyCards/GlossyCards.svelte";
+	import type { IGlossyCard, IGlossyCards } from "$lib/types/glossy-card.types";
+
+    const cards: IGlossyCard[] = [
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+                
+    ]
 
 </script>
 
@@ -14,6 +51,6 @@
                 <hr />
             </h1>
         </div>
-        <GlossyCards />
+        <GlossyCards cards={cards} />
     </Section>
 </Page>
