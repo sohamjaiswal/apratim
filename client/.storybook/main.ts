0a7587d9ac5897648f692f408563e717efc987de
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-02-25 11:47:11 +0530

    fix: it's cucked but it's there

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,15 +1,12 @@
 import type { StorybookConfig } from "@storybook/sveltekit";
 const config: StorybookConfig = {
 	// stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-	stories: [
-		"../src/**/*.mdx",
-		"../src/**/*.stories.@(js|jsx|ts|tsx)",
-		"../src/lib/components/**/**/*.stories.@(ts)"
-	],
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
-		"@storybook/addon-interactions"
+		"@storybook/addon-interactions",
+		"@storybook/addon-svelte-csf"
 	],
 	framework: {
 		name: "@storybook/sveltekit",
