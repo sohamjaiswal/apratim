a26583765292d9e91b42515cfbce09b62c69f574
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-02-25 18:15:46 +0530

    feat: dynamic-blob

--- client/src/lib/components/fragments/GlossyCards/GlossyCards.svelte
+++ client/src/lib/components/fragments/GlossyCards/GlossyCards.svelte
@@ -26,10 +26,9 @@
     <div class="card">
         <div class="card-border" />
         <div class="card-content">
-            <div class="banner" style={`background-image: url(https://images7.alphacoders.com/108/1081933.jpg);`} />
+            <div class="banner" style={`background-image: url(${cardContent.image});`} />
             <div class="content">
-                <h4>CCET Valorant Tourney</h4>
-                <p>Cash prizes upto <strong>INR 10K<strong></p>
+                {@html cardContent.description}
             </div>
         </div>
     </div>
--- client/src/routes/events/+page.svelte
+++ client/src/routes/events/+page.svelte
@@ -1,12 +1,51 @@
 <script lang="ts">
+	import GlossyCards from '$lib/components/fragments/GlossyCards/GlossyCards.svelte';
 
     import Section from "$lib/components/ui/Section/Section.svelte";
 	import Page from "$lib/components/fragments/Page/Page.svelte";
-	import GlossyCards from "$lib/components/fragments/GlossyCards/GlossyCards.svelte";
+	import type { IGlossyCard } from "$lib/types/glossy-card.types";
+
+    let showBlob = true;
+
+    const cards: IGlossyCard[] = [
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+        {
+            image: "https://pic-bstarstatic.akamaized.net/ugc/65d655c68b92563e29212eb4e4cc9b83.jpg",
+            description: "<h4>Harumaki Gohan</h4><hr style='width: 100%' /> <p>Witness the legend.</p>"
+        },
+                
+    ]
 
 </script>
 
-<Page>
+<Page bind:showBlob={showBlob}>
     <Section>
         <div class="heading">
             <h1>
@@ -14,6 +53,16 @@
                 <hr />
             </h1>
         </div>
-        <GlossyCards />
+        <div class="glossyCardContainer" on:mouseenter="{() => showBlob = false}" on:mouseleave="{() => showBlob = true}">
+            <GlossyCards cards={cards} />
+        </div>
     </Section>
 </Page>
+
+<style lang="scss">
+
+    .glossyCardContainer {
+        display: flex;
+    }
+
+</style>
