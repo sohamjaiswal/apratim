36e48b20218102127bb4ef6a187ed65faef066c6
Sahil Garg <contact.sahilgarg@proton.me>
2023-03-14 22:41:57 +0530

    feat: Added goto events page button in landing page.

--- client/src/lib/components/fragments/HomeEvents/HomeEvents.svelte
+++ client/src/lib/components/fragments/HomeEvents/HomeEvents.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import FlipCard from "$lib/components/ui/FlipCard/FlipCard.svelte";
+    import Button from "$lib/components/ui/Button/Button.svelte";
 </script>
 
 <section>
@@ -11,6 +12,9 @@
             <FlipCard title="Technical" subtitle="Tech Events: Where Innovation Thrives" desc="Explore the Latest Technologies, Compete with others and Gain Insights - Technical Events are Your Perfect Opportunity!" src="/assets/events/technical events.jpg" />
             <FlipCard title="Literary" subtitle="Where Words Come to Life: Literary Events" desc="Immerse Yourself in Literature: Discover New Works and Authors at Literary Events!" src="/assets/events/literary events.jpg" />
             <FlipCard title="Cultural" subtitle="Embrace Diversity and Culture at Cultural Events" desc="Are you fascinated by different cultures and traditions and celebrations? Then cultural events are just what you need! These events offer a unique opportunity to immerse yourself in the rich tapestry of cultures and celebrate diversity." src="/assets/events/cultural events.jpg" />
+        </div>
+        <div class="button">
+            <Button variant="secondary" label="Open Events" onClick={() => {alert("Go to events page.")}} size="large"></Button>
         </div>
     </div>
 </section>
